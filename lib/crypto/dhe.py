4d77cff0b3692e7ad5b6b57967bc1db73d56a97b
Brandon Wang <sourbrandon@gmail.com>
2020-08-05 12:50:20 -0700

    use predefined dhe parameters

--- lib/crypto/dhe.py
+++ lib/crypto/dhe.py
@@ -2,30 +2,27 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat, ParameterFormat, load_der_public_key, load_der_parameters
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat, load_der_public_key
 
-KEY_SIZE = 512
-G = 2
 BACKEND = default_backend()
 ENCODING = Encoding.DER
-PARAM_FORMAT = ParameterFormat.PKCS3
 KEY_FORMAT = PublicFormat.SubjectPublicKeyInfo
 INFO = b'smokescreen handshake'
 SHARED_KEY_LEN = 48
 
-def dhe_generate_params():
-    parameters = dh.generate_parameters(generator=G, key_size=KEY_SIZE, backend=BACKEND)
-    return parameters.parameter_bytes(ENCODING, PARAM_FORMAT)
+p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
+g = 2
 
-def dhe_generate_key(params_serialized):
-    dh_params = load_der_parameters(params_serialized, backend=BACKEND)
-    return dh_params.generate_private_key()
+params_numbers = dh.DHParameterNumbers(p,g)
+PARAMETERS = params_numbers.parameters(BACKEND)
+
+def dhe_generate_key(parameters):
+    return parameters.generate_private_key()
 
 def dhe_serialize(key):
     return key.public_key().public_bytes(ENCODING, KEY_FORMAT)
 
-def dhe_generated_shared(key, peer_serialized, length):
-    peer_key = load_der_public_key(peer_serialized, backend=BACKEND)
+def dhe_generated_shared(key, peer_key, length):
     shared_key = key.exchange(peer_key)
     return HKDF(algorithm=hashes.SHA256(),
         length=length,
@@ -33,36 +30,24 @@ def dhe_generated_shared(key, peer_serialized, length):
         info=INFO,
         backend=BACKEND).derive(shared_key)
 
-def format_client_request(params_ser, public_key_ser):
-    params_len = len(params_ser)
-    return f'{params_len}\n'.encode() + params_ser + public_key_ser
-
-def extract_from_client_request(request):
-    params_len = request.split(b'\n')[0].decode()
-    params_start = len(params_len) + 1 # +1 because of new line
-    key_start = params_start + int(params_len)
-    params_ser = request[params_start: key_start]
-    key_ser = request[key_start:]
-
-    return params_ser, key_ser
-
 def client_dhe_request():
-    params_ser = dhe_generate_params()
-    private_key = dhe_generate_key(params_ser)
+    private_key = dhe_generate_key(PARAMETERS)
     public_key_ser = dhe_serialize(private_key)
 
-    return private_key, format_client_request(params_ser, public_key_ser)
+    return private_key, public_key_ser
 
 def server_dhe_response(client_req):
-    params_ser, client_key_ser = extract_from_client_request(client_req)
-    private_key = dhe_generate_key(params_ser)
+    client_key = load_der_public_key(client_req, backend=BACKEND)
+    parameters = client_key.parameters()
+    private_key = dhe_generate_key(parameters)
     public_key_ser = dhe_serialize(private_key)
-    shared_key = dhe_generated_shared(private_key, client_key_ser, SHARED_KEY_LEN)
+    shared_key = dhe_generated_shared(private_key, client_key, SHARED_KEY_LEN)
 
     return shared_key, public_key_ser
 
 def client_dhe_finish(private_key, server_resp):
-    shared_key = dhe_generated_shared(private_key, server_resp, SHARED_KEY_LEN)
+    server_key = load_der_public_key(server_resp, backend=BACKEND)
+    shared_key = dhe_generated_shared(private_key, server_key, SHARED_KEY_LEN)
     return shared_key
 
 if __name__ == '__main__':
